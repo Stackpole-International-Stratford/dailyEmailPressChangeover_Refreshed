f247110f20e28fd76bd238b58109d28d373a6a19
Chris Strutton <chris@rodandfly.ca>
2021-10-22 15:05:49 -0400

    to run main.py at between 7 and 7:30 AM

--- crontest.py
+++ crontest.py
@@ -4,5 +4,5 @@ import socket
 
 print(f'Cron job has run at {datetime.datetime.now()}')
 
-address = socket.gethostbyname('mesg06.stackpole.ca')
-print(address)
+# test network connectivity
+print(socket.gethostbyname('mesg06.stackpole.ca'))
