69ca428cc7a9d5884227844d7c5aa51c8fd27944
Chris Strutton <chris@rodandfly.ca>
2021-10-22 13:14:02 -0400

    test network connectivity

--- crontest.py
+++ crontest.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 import datetime
+import socket
+
 print(f'Cron job has run at {datetime.datetime.now()}')
+
+address = socket.gethostbyname('mesg06.stackpole.ca')
+print(address)
