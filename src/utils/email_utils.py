1f7fb9728a3e7d6b3a6b2b9591dd3979e58d05c0
Tyler Careless <tyler444c@gmail.com>
2024-05-23 08:52:46 -0400

    Refactored code to work with daily email boiler plate. Now need to add extensive comments and ask about styling the html a bit. Nearly finished.

--- src/utils/email_utils.py
+++ src/utils/email_utils.py
@@ -1,5 +1,3 @@
-# src/utils/email_utils.py
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -8,9 +6,7 @@ import os
 
 logger = setup_logger()
 
-
 def load_email_config():
-    # Load email configuration from environment variables
     email_config = {
         'server': os.getenv('EMAIL_SERVER'),
         'from': os.getenv('EMAIL_FROM'),
@@ -19,26 +15,11 @@ def load_email_config():
     }
     return email_config
 
-
 def get_email_list():
-    # Fetch the email list from the environment variable, split by comma
     email_list = os.getenv('EMAIL_LIST', '').split(',')
     return email_list
 
 def send_email(report, email_config):
-    # """
-    # Send an email with a report as HTML content using SMTP configuration provided.
-    
-    # Parameters:
-    # report (str): The HTML content of the report to be sent.
-    # email_config (dict): A dictionary containing SMTP configuration including server, sender, recipient(s), and subject.
-    
-    # Returns:
-    # None
-    
-    # Raises:
-    # Exception: If there's an issue with SMTP or an unexpected error occurs during email sending.
-    # """
     message = MIMEMultipart("alternative")
     message["Subject"] = email_config['subject']
     message["From"] = email_config['from']
@@ -46,21 +27,21 @@ def send_email(report, email_config):
     msg_body = MIMEText(report, "html")
     message.attach(msg_body)
     
-    # Try to connect to the SMTP server and send the email.
+    server = None
     try:
         server = smtplib.SMTP(email_config['server'])
         server.sendmail(email_config['from'], email_config['to'], message.as_string())
-        server.quit()  # Ensure the connection is closed after sending the email.
         logger.info("Email sent successfully to {}".format(email_config['to']))
     except smtplib.SMTPException as e:
-        # Log SMTP specific exceptions, e.g., authentication issues, connection refused.
         logger.error(f"Failed to send email: {e}")
         raise Exception("Failed to send email due to SMTP issue.") from e
     except Exception as e:
-        # Log any other unexpected exceptions that may occur.
         logger.error(f"An unexpected error occurred: {e}")
         raise Exception("An unexpected error occurred when sending an email.") from e
     finally:
-        # Ensure the server connection is closed even if an exception occurs.
-        server.quit()
+        if server:
+            try:
+                server.quit()
+            except smtplib.SMTPServerDisconnected:
+                pass  # Connection was already closed
 
--- src/utils/report_utils.py
+++ src/utils/report_utils.py
@@ -1,115 +1,97 @@
 from datetime import datetime, timedelta
 from utils.logging_utils import setup_logger
+import mysql.connector
 import jinja2
 import traceback
 import os
-import sys
 
 logger = setup_logger()
 
-def shift_times(date, date_offset=0):
-    # """
-    # Shifts the given date by a specified number of days and returns the start and end dates.
+start_hour = 6
 
-    # Parameters:
-    # - date: The base date for shifting.
-    # - date_offset (optional): The number of days to offset the date. Defaults to 0.
-
-    # Returns:
-    # - start_date: The start date after shifting.
-    # - end_date: The end date after shifting.
-    # """
-
-    # end_date is this morning at 7am
-    end_date = date.replace(hour=7, minute=0, second=0, microsecond=0)
+def shift_times(date, date_offset=0, start_hour=6):
+    # end_date is today at {start_hour}
+    end_date = date.replace(hour=start_hour, minute=0, second=0, microsecond=0)
     # adjust end_date by date_offset days
     end_date = end_date - timedelta(days=date_offset)
-    # start_date is yesterday morning at 7am
+    # start_date is the day before at {start_hour}
     start_date = end_date - timedelta(hours=24)
     end_date = end_date - timedelta(seconds=1)
     return start_date, end_date
 
+def completed_changeovers(start, end):
+    results = []
+    db_config = {
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'database': 'prodrptdb',
+        # 'raise_on_warnings': True
+    }
+    with mysql.connector.connect(**db_config) as cnx:
+        cursor = cnx.cursor()
 
+        query = ("SELECT * FROM pr_downtime1 "
+                "WHERE priority = -2 "
+                "AND ( completedtime BETWEEN %s AND %s)")
 
+        cursor.execute(query, (start, end))
 
-def get_data(start, end):
-#   """
-#     Retrieve data for report generation within the specified time range. This is where you would query the database for data, by replacing the dummy data with
-#     an actual function to do so.
-    
-#     Parameters:
-#     start (datetime): The start time for data retrieval.
-#     end (datetime): The end time for data retrieval.
-    
-#      Returns:
-#       - list: A list of dictionaries, each containing information about parts.
-#         Each dictionary could have the following keys for example:
-#         - 'part_number': The part number.
-#         - 'good': The count of good parts.
-#         - 'reject': A dictionary containing information about rejected parts.
-#             Each key in the 'reject' dictionary corresponds to a type of rejection,
-#             and its value is a dictionary containing the label and count of rejections.
-#     Example:
-#     [
-#         {'part_number': '1234', 'good': 100, 'reject': {'spotface': {'label': 'Spot Face', 'count': 5}}},
-#         {'part_number': '5678', 'good': 200, 'reject': {'media': {'label': 'Media', 'count': 3}}},
-#         ...
-#     ]
-#     """   
+        for row in cursor:
+            record = {
+                'machine': row[0],
+                'problem': row[1],
+                'comments': row[8],
+                'updatedtime': row[10],
+                'completedtime': row[7],
+                'changeovertime': row[15],
+                'setupdelta': row[15] - row[10],
+                'dialindelta': row[7] - row[15],
+            }
 
-    # Dummy data
-    data = [
-        {'part_number': '1234', 'good': 100, 'reject': {'spotface': {'label': 'Spot Face', 'count': 5}}},
-        {'part_number': '5678', 'good': 200, 'reject': {'media': {'label': 'Media', 'count': 3}}},
-        {'part_number': '2323', 'good': 300, 'reject': {}},
-        {'part_number': '1234', 'good': 100, 'reject': {'spotface': {'label': 'Spot Face', 'count': 5}}},
-        {'part_number': '5678', 'good': 200, 'reject': {'media': {'label': 'Media', 'count': 3}}},
-        {'part_number': '2323', 'good': 300, 'reject': {}},
-        {'part_number': '1234', 'good': 100, 'reject': {'spotface': {'label': 'Spot Face', 'count': 5}}},
-        {'part_number': '5678', 'good': 200, 'reject': {}},
-        {'part_number': '2323', 'good': 300, 'reject': {}},
-        {'part_number': '1234', 'good': 100, 'reject': {'spotface': {'label': 'Spot Face', 'count': 5}}},
-        {'part_number': '7889', 'good': 150, 'reject': {'burrs': {'label': 'Burrs', 'count': 2}}},
-        {'part_number': '9988', 'good': 250, 'reject': {'warp': {'label': 'Warp', 'count': 1}}},
-        {'part_number': '5467', 'good': 350, 'reject': {'crack': {'label': 'Crack', 'count': 4}}},
-        {'part_number': '2143', 'good': 120, 'reject': {'spotface': {'label': 'Spot Face', 'count': 3}}},
-        {'part_number': '8765', 'good': 220, 'reject': {'media': {'label': 'Media', 'count': 6}}},
-        {'part_number': '3434', 'good': 330, 'reject': {'porosity': {'label': 'Porosity', 'count': 7}}},
-        {'part_number': '1212', 'good': 140, 'reject': {'spotface': {'label': 'Spot Face', 'count': 8}}},
-        {'part_number': '4545', 'good': 230, 'reject': {'deformation': {'label': 'Deformation', 'count': 5}}},
-        {'part_number': '6767', 'good': 340, 'reject': {'paint': {'label': 'Paint Defect', 'count': 2}}},
-        {'part_number': '8989', 'good': 160, 'reject': {'spotface': {'label': 'Spot Face', 'count': 1}}},
-        {'part_number': '1010', 'good': 210, 'reject': {'media': {'label': 'Media', 'count': 3}}},
-        {'part_number': '3232', 'good': 310, 'reject': {}},
-        {'part_number': '5432', 'good': 190, 'reject': {'spotface': {'label': 'Spot Face', 'count': 6}}},
-        {'part_number': '7865', 'good': 205, 'reject': {'crack': {'label': 'Crack', 'count': 2}}},
-        {'part_number': '3498', 'good': 305, 'reject': {'media': {'label': 'Media', 'count': 5}}},
-        {'part_number': '2124', 'good': 115, 'reject': {'spotface': {'label': 'Spot Face', 'count': 3}}},
-        {'part_number': '5656', 'good': 225, 'reject': {'deformation': {'label': 'Deformation', 'count': 4}}},
-        {'part_number': '7373', 'good': 335, 'reject': {'paint': {'label': 'Paint Defect', 'count': 1}}},
-        {'part_number': '9191', 'good': 155, 'reject': {'spotface': {'label': 'Spot Face', 'count': 2}}},
-    ]
+            results.append(record)
+    return results
+
+def pending_changeovers(start, end):
+    results = []
+    db_config = {
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'database': 'prodrptdb',
+        # 'raise_on_warnings': True
+    }
+    with mysql.connector.connect(**db_config) as cnx:
+        cursor = cnx.cursor()
+
+        query = "SELECT * FROM pr_downtime1 WHERE priority = -1;"
+
+        cursor.execute(query)
+
+        for row in cursor:
+            record = {
+                'machine': row[0],
+                'problem': row[1],
+                'called4helptime': row[2],
+            }
+
+            results.append(record)
+    return results
+
+def get_report_data(start, end):
+    pending_list = pending_changeovers(start, end)
+    completed_list = completed_changeovers(start, end)
+    data = {
+        'start': start,
+        'end': end,
+        'completed_list': completed_list,
+        'pending_list': pending_list,
+    }
     return data
 
-
-
 def render_report(data, start, end):
-    # """
-    # Render a report HTML using Jinja2 templates based on the provided data, start time, and end time.
-    # The function loads a template from the 'templates' directory, injects the data into the template,
-    # and returns the rendered HTML content.
-    
-    # Parameters:
-    # data (list of dict): The data to be included in the report, typically containing information about parts.
-    # start (datetime): The start time for the report period.
-    # end (datetime): The end time for the report period.
-    
-    # Returns:
-    # str: The rendered HTML content of the report.
-    
-    # Raises:
-    # jinja2.TemplateError: If there is an error during template rendering.
-    # """
     try:
         # Get the directory path where main.py is located
         current_directory = os.path.dirname(__file__)
@@ -118,7 +100,10 @@ def render_report(data, start, end):
         template_path = os.path.join(current_directory, '..', 'templates')
 
         # Load the template from the templates directory
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path))
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(template_path),
+            extensions=["jinja2_humanize_extension.HumanizeExtension"]
+        )
         template = env.get_template('template.html')
         return template.render(data=data, start=start, end=end)
     except Exception as e:
